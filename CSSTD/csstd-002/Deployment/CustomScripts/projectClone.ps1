1da12eff12f6cdb7decfab51697f08534ad21fa0
MGrasdal <martin_grasdal@hotmail.com>
2021-03-02 13:20:59 -0700

    Update projectClone.ps1

--- CSSTD/csstd-002/Deployment/CustomScripts/projectClone.ps1
+++ CSSTD/csstd-002/Deployment/CustomScripts/projectClone.ps1
@@ -2,4 +2,4 @@
 d:
 md challenge
 cd challenge
-git clone https://github.com/LODSContent/ChallengeLabs_Resources/tree/master/CSSTD/csstd-002-project .
+git clone https://github.com/LODSContent/LODSOC_csstd-002-project .
