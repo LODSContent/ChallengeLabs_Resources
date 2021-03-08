029669bcec011f37d2cee03731a7da35bc643830
MGrasdal <martin_grasdal@hotmail.com>
2021-03-08 09:11:57 -0700

    Add files via upload

---
+++ CSSTD/csstd_v3/CSSTDEValuationEngine/NoSQLEvaluations.cs
@@ -0,0 +1,99 @@
+﻿using CSSTDModels;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CSSTDEvaluation
+{
+    public class NoSQLEvaluationProcessor
+    {
+        private SampleData sampleData;
+        public NoSQLEvaluationProcessor(string baseFolder, string encryptionKey)
+        {
+            sampleData = new SampleData(baseFolder);
+        }
+        public NoSQLEvaluationProcessor() { }
+
+        public EvaluationResult<ProductDocument> CosmosDBSQLUpload(ICosmosDBSQLContext context, string containerName)
+        {
+            var result = new EvaluationResult<ProductDocument>();
+            var data = sampleData.ProductData();
+            try
+            {
+                Task t = Task.Run(async () =>
+                {
+                    await context.CreateCollection(containerName);
+                    await context.UploadDocuments(data, containerName);
+                });
+                t.Wait();
+
+            }
+            catch (Exception ex)
+            {
+                result.Code = 2;
+                result.Text = $"There was an error uploading product documents to Cosmos DB: {ex.Message}";
+            }
+
+            return result;
+        }
+        public EvaluationResult<ProductDocument> CosmosDBSQLDownload(ICosmosDBSQLContext context, string containerName)
+        {
+            var result = new EvaluationResult<ProductDocument>();
+            try
+            {
+                result.Results = context.GetDocuments(containerName);
+                result.Code = result.Results.Count > 0 ? 0 : 1;
+                result.Text = result.Results.Count > 0 ? "Successfully returned product documents from Cosmos DB" : "Did not return any documents from Cosmos DB";
+
+            }
+            catch (Exception ex)
+            {
+                result.Code = 2;
+                result.Text = $"There was an error downloading product documents from Cosmos DB: {ex.Message}";
+            }
+            return result;
+        }
+
+        public EvaluationResult<IProductMention> CosmosDBTableUpload(ICosmosDBTableContext context, string tableName)
+        {
+            var result = new EvaluationResult<IProductMention>();
+            try
+            {
+                var data = sampleData.ProductMentionData();
+                context.CreateTable(tableName);
+                context.LoadMentions(data, tableName);
+                result.Code = 0;
+                result.Text = "Successfully uploaded table data to Cosmos DB account.";
+            }
+            catch (Exception ex)
+            {
+                result.Code = 2;
+                result.Text = $"There was an error uploading table data: {ex.Message}";
+            }
+
+
+            return result;
+        }
+
+        public EvaluationResult<IProductMention> CosmosDBTableDownload(ICosmosDBTableContext context, string tableName)
+        {
+            var result = new EvaluationResult<IProductMention>();
+            try
+            {
+                result.Results = new List<IProductMention>(context.GetMentions(tableName));
+                result.Code = result.Results.Count > 0 ? 0 : 1;
+                result.Text = result.Code == 0 ? "Successfully downloaded table data from Cosmos DB account" :
+                    "There were no errors, but no records were returned from Cosmos DB table.";
+            }
+            catch (Exception ex)
+            {
+                result.Code = 2;
+                result.Text = $"There was an error retrieving tabular data from Cosmos DB: {ex.Message}";
+            }
+
+            return result;
+        }
+
+    }
+
+}
---
+++ CSSTD/csstd_v3/CSSTDEValuationEngine/SampleData.cs
@@ -0,0 +1,88 @@
+﻿using CSSTDModels;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace CSSTDEvaluation
+{
+    public class SampleData
+    {
+        public string dataFolder;
+        public SampleData(string dataFolder)
+        {
+            this.dataFolder = dataFolder;
+        }
+        public SampleData() { }
+
+        public List<BlobFileData> BlobData()
+        {
+            var files = new List<BlobFileData>();
+            string[] fileNames = { "doors.jpg", "labondemand.jpg", "lodslogo.png" };
+            string[][] tags = { new string[] { "Abstract", "Physical" }, new string[] { "Learning", "Person" }, new string[] { "Corporate", "Logo" } };
+            for (int i = 0; i < 3; i++)
+            {
+                string filePath = $"{dataFolder}\\{fileNames[i]}";
+                files.Add(new BlobFileData
+                {
+                    Contents = File.ReadAllBytes(filePath),
+                    Name = fileNames[i],
+                    Tags = new List<string>(tags[i])
+                });
+
+            }
+            return files;
+        }
+        public List<CustomerData> CustomerData()
+        {
+            return JsonConvert.DeserializeObject<List<CustomerData>>(File.ReadAllText($"{dataFolder}\\Customers.json"));
+        }
+        public List<VendorData> VendorData()
+        {
+            return JsonConvert.DeserializeObject<List<VendorData>>(File.ReadAllText($"{dataFolder}\\Vendors.json"));
+        }
+        public List<ProductDocument> ProductData()
+        {
+            return JsonConvert.DeserializeObject<List<ProductDocument>>(File.ReadAllText($"{dataFolder}\\Products.json"));
+        }
+
+        public List<IProductMention> ProductMentionData()
+        {
+            List<IProductMention> results = new List<IProductMention>();
+            string[] platforms = new string[] { "Twitter", "LinkedIn", "Reddit", "Instagram" };
+            string[] mentions = new string[] {
+                "We just tried %product% and it was amazing #%industry%",
+                "Shout out to %product%! It's amazing",
+                "I learned a lot from %product%! Kudos to LODS."};
+            var products = ProductData();
+            var rnd = new Random();
+            foreach (var product in products)
+            {
+                foreach (var platform in platforms)
+                {
+                    foreach (var mention in mentions)
+                    {
+                        results.Add(new ProductMention
+                        {
+                            MentionID = Guid.NewGuid().ToString(),
+                            Mention = mention.Replace("%product%", product.Name).Replace("%industry%", product.Industry),
+                            MentionedAt = DateTime.Now.AddDays(-rnd.Next(1, 100)).AddMinutes(-rnd.Next(1200)).ToString(),
+                            Platform = platform,
+                            Product = product.Name
+                        });
+                    }
+                }
+            }
+            return results;
+        }
+    }
+
+    internal class ProductMention : IProductMention
+    {
+        public string MentionID { get; set; }
+        public string Mention { get; set; }
+        public string MentionedAt { get; set; }
+        public string Platform { get; set; }
+        public string Product { get; set; }
+    }
+}
