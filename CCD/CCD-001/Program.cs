a90f96226f5a3e0ada43a99271ab395ca040f80a
John Rice <84397292+JohnR-LoD@users.noreply.github.com>
2022-05-09 14:33:31 -0400

    Fix case errors (#2)
    * Added CCD-001
    Added CCD-002
    Added CCD-003 cd
    
    * Added CCD-001
    Added CCD-002
    Added CCD-003 cd
    
    * Update mysqlTest.cs
    
    * Update mysqlTest.cs
    
    * Fix case errors in namespace and class fields

--- CCD/CCD-001/Program.cs
+++ CCD/CCD-001/Program.cs
@@ -2,7 +2,7 @@
 using System.IO;
 using System.Text.Json;
 
-namespace Labfiles
+namespace labfiles
 {
     class Program
     {
--- CCD/CCD-001/fileCompletedCode.cs
+++ CCD/CCD-001/fileCompletedCode.cs
@@ -4,7 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text.Json;
 
-namespace Labfiles
+namespace labfiles
 {
     public class FileCode
     {
