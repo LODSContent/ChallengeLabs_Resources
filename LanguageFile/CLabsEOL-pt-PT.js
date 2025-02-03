d4cd50d31d741d65bf8a874638ba8fcbbd7bcc9e
markmorganskillable <127998103+markmorganskillable@users.noreply.github.com>
2025-02-03 09:43:17 -0500

    Add files via upload

---
+++ LanguageFile/CLabsEOL-pt-PT.js
@@ -0,0 +1,132 @@
+window.strings = {
+	"elements": {
+		"excelled": "Áreas em que você se destacou",
+		"growth": "Áreas com potencial de melhoria",
+		"title": "Avaliações",
+		"feedback": "Comentários do Laboratório de Desafios",
+		"print": "Impressão"
+	},    
+	"summary": [
+        {
+            "maxScore": 100,
+            "minScore": 90,
+            "headings": [
+                "Mandou bem!",
+                "Bem merecido!",
+                "Ótimo trabalho!",
+                "Parabéns!",
+                "Trabalho incrível!"
+            ],
+            "captions": [
+                "Faça espaço na prateleira de troféus!",
+                "Parabéns pelo seu sucesso!",
+                "Aproveite o sucesso que você trabalhou tanto para alcançar!",
+                "Sua dedicação e trabalho duro realmente valeram a pena!",
+                "Você vai querer colocar isso na geladeira!"
+            ],
+            "color":"#6FCF97"
+        },
+        {
+            "maxScore": 89,
+            "minScore": 75,
+            "headings":[
+                "Ótimo começo!",
+                "Está indo bem!",
+                "Quase perfeito!",
+                "Difícil de superar!",
+                "Muito bom!"
+            ],
+            "captions":[
+                "Você fez bem! Tente novamente para aperfeiçoar sua habilidade.",
+                "Você alcançou muito. Tente mais uma vez para aperfeiçoar as coisas.",
+                "A prática leva à perfeição. Tente novamente.",
+                "Não se contente com o bastante bom. Tente novamente para ser incrível!",
+                "Tente mais uma vez e achamos que você vai aperfeiçoar isso!"
+            ],
+            "color":"#93D63E"
+        },
+        {
+            "maxScore": 74,
+            "minScore": 61,
+            "headings":[
+                "Bom começo!",
+                "Acabou de conseguir!",
+                "Virando a esquina!",
+                "Chegando lá!",
+                "Alcance mais alto!"
+            ],
+            "captions":[
+                "Você tem uma boa base aqui. Por que não tentar novamente para aprimorar suas habilidades?",
+                "Você acabou de passar da linha de chegada. Talvez queira praticar um pouco mais.",
+                "Parece que você acabou de superar o obstáculo. Tente novamente para realmente ganhar tração.",
+                "Você sabe que pode fazer isso, agora tente novamente e mostre a todos que pode fazer ainda melhor!",
+                "Trabalhe duro. Pratique muito. As possibilidades são ilimitadas!"
+            ],
+            "color":"#F2994A"
+        },
+        {
+            "maxScore": 60,
+            "minScore": 0,
+            "headings":[
+                "Precisa de ajuda?",
+                "Continue!",
+                "Você quer isso!",
+                "Mire nas estrelas!",
+                "É possível!",
+                "Você consegue!",
+                "Supere as expectativas!"
+            ],
+            "captions":[
+                "Faça uma pausa e tente novamente. Você vai conseguir.",
+                "Nunca deixe nada te derrubar. Tente novamente!",
+                "Descubra qual é o seu destino e então faça isso!",
+                "Falhe rápido e falhe para frente! Você consegue.",
+                "Nada é impossível. A própria palavra diz 'Eu sou possível!'",
+                "Recupere-se disso e tente novamente!",
+                "Comece de novo e faça um novo final, melhor!"
+            ],
+            "color":"#EB5757"
+        }
+    ],
+    "activities": [
+        {
+            "maxScore": 100,
+            "minScore": 90,
+            "messages": [
+                "Você completou esta área com grande sucesso.",
+                "Sem dúvida, nesta seção você sabe o que está fazendo.",
+                "Depois de completar esta seção, é óbvio que você sabe do que está falando.",
+                "Você completou esta seção com facilidade.",
+                "Parece que você tem um forte domínio deste material.",
+                "Você mostrou suas habilidades nesta área e terminou com sucesso.",
+                "Você foi realmente forte nesta área."
+            ]
+        },
+        {
+            "maxScore": 89,
+            "minScore": 70,
+            "messages": [
+                "Você demonstrou competência nesta área, mas talvez queira tentar novamente para uma melhor compreensão.",
+                "Suas habilidades nesta área estão crescendo, mas ainda precisam de melhorias para alcançar seu objetivo.",
+                "Talvez você queira se concentrar um pouco aqui, há mais espaço para crescer nesta área.",
+                "Você começou bem, mas há espaço para melhorar nesta seção.",
+                "Você teve algum sucesso nesta seção, mas talvez queira aprimorar suas habilidades aqui.",
+                "Se você revisar o material desta área, achamos que você fará ainda melhor na próxima vez.",
+                "Seu desempenho nesta seção foi suficiente, mas poderia ser mais forte."
+            ]
+        },
+        {
+            "maxScore": 69,
+            "minScore": 0,
+            "messages": [
+                "As competências que você mostrou nesta área poderiam ser mais fortes.",
+                "Parece que você pode precisar de uma pausa para se reagrupar e tentar novamente.",
+                "Há bastante espaço para você melhorar nesta seção.",
+                "Esta área não foi a mais forte, então tente novamente.",
+                "Você deve passar mais tempo revisando esta área e tentar novamente.",
+                "Verifique seu material de origem e tente novamente, há espaço para melhorar.",
+                "Você precisa trabalhar nesta área para transformá-la em uma vitória."
+            ]
+        }
+    ]
+}
