e0c3a578382ee71806e1add3f8513c7dc1842dd5
markmorganskillable <127998103+markmorganskillable@users.noreply.github.com>
2025-02-04 18:51:02 -0500

    Add files via upload

---
+++ LanguageFiles/CLabsEOL-ML.js
@@ -0,0 +1,132 @@
+window.strings = {
+	"elements": {
+		"excelled": "<language>Areas where you excelled</language>",
+		"growth": "<language>Areas where you have room to grow</language>",
+		"title": "<language>Assessments</language>",
+		"feedback": "<language>Challenge Labs Feedback</language>",
+		"print": "<language>Print</language>"
+	},
+    "summary": [
+        {
+            "maxScore": 100,
+            "minScore": 90,
+            "headings": [
+                "<language>Nailed it!</language>",
+                "<language>Well deserved!</language>",
+                "<language>Great job!</language>",
+                "<language>Congratulations!</language>",
+                "<language>Amazing work!</language>"
+            ],
+            "captions": [
+                "<language>Clear out some room in the trophy case!</language>",
+                "<language>Congratulations on your success!</language>",
+                "<language>Enjoy the success you've worked so hard to achieve!</language>",
+                "<language>Your dedication and hard work really paid off!</language>",
+                "<language>You'll want to put this on the fridge!</language>"
+            ],
+            "color":"#6FCF97"
+        },
+        {
+            "maxScore": 89,
+            "minScore": 75,
+            "headings":[
+                "<language>Great start!</language>",
+                "<language>Doing well!</language>",
+                "<language>Nearly perfected!</language>",
+                "<language>Hard to beat!</language>",
+                "<language>So good!</language>"
+            ],
+            "captions":[
+                "<language>You did well! Try again to perfect your skill.</language>",
+                "<language>You've accomplished a lot. Give it one more shot to perfect things.</language>",
+                "<language>Practice makes perfect. Give it another shot.</language>",
+                "<language>Don't settle for pretty good. Try again for amazing!</language>",
+                "<language>Give it one more try and we think you'll perfect this!</language>"
+            ],
+            "color":"#93D63E"
+        },
+        {
+            "maxScore": 74,
+            "minScore": 61,
+            "headings":[
+                "<language>Good start!</language>",
+                "<language>Just made it!</language>",
+                "<language>Turning the corner!</language>",
+                "<language>Getting there!</language>",
+                "<language>Reach higher!</language>"
+            ],
+            "captions":[
+                "<language>You've got a good base here. Why not try again to polish your skills?</language>",
+                "<language>You just made it past the finish line. You might want to practice some more.</language>",
+                "<language>Looks like you just made it over the hump. Try again to really gain some traction.</language>",
+                "<language>You know you can do it, now try again, and show everyone you can do even better!</language>",
+                "<language>Work hard. Practice hard. Possibilities are limitless!</language>"
+            ],
+            "color":"#F2994A"
+        },
+        {
+            "maxScore": 60,
+            "minScore": 0,
+            "headings":[
+                "<language>Need some help?</language>",
+                "<language>Keep going!</language>",
+                "<language>You want this!</language>",
+                "<language>Aim for the stars!</language>",
+                "<language>It is possible!</language>",
+                "<language>You can do it!</language>",
+                "<language>Exceed expectations!</language>"
+            ],
+            "captions":[
+                "<language>Take a break and try again. You'll get there.</language>",
+                "<language>Never let anything bring you down. Give it another shot!</language>",
+                "<language>Find out what your destiny is, and then do it!</language>",
+                "<language>Fail fast and fail forward! You got this.</language>",
+                "<language>Nothing is impossible. The word itself says \"I'm Possible!\"</language>",
+                "<language>Bounce back from this and try again!</language>",
+                "<language>Start again and make a new, better, ending!</language>"
+            ],
+            "color":"#EB5757"
+        }
+    ],
+    "activities": [
+        {
+            "maxScore": 100,
+            "minScore": 90,
+            "messages": [
+                "<language>You completed this area with great success.</language>",
+                "<language>No doubt about it, in this section you know your stuff.</language>",
+                "<language>After completing this section, itâ€™s obvious you know what you're talking about.</language>",
+                "<language>You completed this section with ease.</language>",
+                "<language>Looks like you have a strong grasp on this material.</language>",
+                "<language>You showed your skills in this area and finished with success.</language>",
+                "<language>You were really strong in this area.</language>"
+            ]
+        },
+        {
+            "maxScore": 89,
+            "minScore": 70,
+            "messages": [
+                "<language>You demonstrated competency in this area, but you might want to try again for a better understanding.</language>",
+                "<language>Your skills in this area are growing, but still need some improvement to reach your goal.</language>",
+                "<language>You might want to focus a bit here, thereâ€™s more room to grow in this area.</language>",
+                "<language>You're off to a good start, but thereâ€™s room to improve in this section.</language>",
+                "<language>You had some success in this section, but you may want to polish your skills here.</language>",
+                "<language>If you review the material for this area, we think you'll do even better next time.</language>",
+                "<language>Your performance in this section was sufficient but could be stronger.</language>"
+            ]
+        },
+        {
+            "maxScore": 69,
+            "minScore": 0,
+            "messages": [
+                "<language>The competencies you showed in this area could be stronger.</language>",
+                "<language>Looks like you could use a break to regroup and try again.</language>",
+                "<language>Thereâ€™s a fair amount of room for you to improve in this section.</language>",
+                "<language>This area was not the strongest, so give it another shot.</language>",
+                "<language>You should spend some more time reviewing this area and try again.</language>",
+                "<language>Check your source material and try again, there's room to improve.</language>",
+                "<language>You need to work on this area to turn it into a victory.</language>"
+            ]
+        }
+    ]
+}
