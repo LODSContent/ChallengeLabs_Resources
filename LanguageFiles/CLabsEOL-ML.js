bc30583b1b278aba70943b46f31043696fb7e183
markmorganskillable <127998103+markmorganskillable@users.noreply.github.com>
2025-02-07 18:24:29 -0500

    Update CLabsEOL-ML.js

--- LanguageFiles/CLabsEOL-ML.js
+++ LanguageFiles/CLabsEOL-ML.js
@@ -1,28 +1,28 @@
 window.strings = {
 	"elements": {
-		"excelled": "<language>Areas where you excelled</language>",
-		"growth": "<language>Areas where you have room to grow</language>",
-		"title": "<language>Assessments</language>",
-		"feedback": "<language>Challenge Labs Feedback</language>",
-		"print": "<language>Print</language>"
+		"excelled": "<x-lt>Areas where you excelled</x-lt>",
+		"growth": "<x-lt>Areas where you have room to grow</x-lt>",
+		"title": "<x-lt>Assessments</x-lt>",
+		"feedback": "<x-lt>Challenge Labs Feedback</x-lt>",
+		"print": "<x-lt>Print</x-lt>"
 	},
     "summary": [
         {
             "maxScore": 100,
             "minScore": 90,
             "headings": [
-                "<language>Nailed it!</language>",
-                "<language>Well deserved!</language>",
-                "<language>Great job!</language>",
-                "<language>Congratulations!</language>",
-                "<language>Amazing work!</language>"
+                "<x-lt>Nailed it!</x-lt>",
+                "<x-lt>Well deserved!</x-lt>",
+                "<x-lt>Great job!</x-lt>",
+                "<x-lt>Congratulations!</x-lt>",
+                "<x-lt>Amazing work!</x-lt>"
             ],
             "captions": [
-                "<language>Clear out some room in the trophy case!</language>",
-                "<language>Congratulations on your success!</language>",
-                "<language>Enjoy the success you've worked so hard to achieve!</language>",
-                "<language>Your dedication and hard work really paid off!</language>",
-                "<language>You'll want to put this on the fridge!</language>"
+                "<x-lt>Clear out some room in the trophy case!</x-lt>",
+                "<x-lt>Congratulations on your success!</x-lt>",
+                "<x-lt>Enjoy the success you've worked so hard to achieve!</x-lt>",
+                "<x-lt>Your dedication and hard work really paid off!</x-lt>",
+                "<x-lt>You'll want to put this on the fridge!</x-lt>"
             ],
             "color":"#6FCF97"
         },
@@ -30,18 +30,18 @@ window.strings = {
             "maxScore": 89,
             "minScore": 75,
             "headings":[
-                "<language>Great start!</language>",
-                "<language>Doing well!</language>",
-                "<language>Nearly perfected!</language>",
-                "<language>Hard to beat!</language>",
-                "<language>So good!</language>"
+                "<x-lt>Great start!</x-lt>",
+                "<x-lt>Doing well!</x-lt>",
+                "<x-lt>Nearly perfected!</x-lt>",
+                "<x-lt>Hard to beat!</x-lt>",
+                "<x-lt>So good!</x-lt>"
             ],
             "captions":[
-                "<language>You did well! Try again to perfect your skill.</language>",
-                "<language>You've accomplished a lot. Give it one more shot to perfect things.</language>",
-                "<language>Practice makes perfect. Give it another shot.</language>",
-                "<language>Don't settle for pretty good. Try again for amazing!</language>",
-                "<language>Give it one more try and we think you'll perfect this!</language>"
+                "<x-lt>You did well! Try again to perfect your skill.</x-lt>",
+                "<x-lt>You've accomplished a lot. Give it one more shot to perfect things.</x-lt>",
+                "<x-lt>Practice makes perfect. Give it another shot.</x-lt>",
+                "<x-lt>Don't settle for pretty good. Try again for amazing!</x-lt>",
+                "<x-lt>Give it one more try and we think you'll perfect this!</x-lt>"
             ],
             "color":"#93D63E"
         },
@@ -49,18 +49,18 @@ window.strings = {
             "maxScore": 74,
             "minScore": 61,
             "headings":[
-                "<language>Good start!</language>",
-                "<language>Just made it!</language>",
-                "<language>Turning the corner!</language>",
-                "<language>Getting there!</language>",
-                "<language>Reach higher!</language>"
+                "<x-lt>Good start!</x-lt>",
+                "<x-lt>Just made it!</x-lt>",
+                "<x-lt>Turning the corner!</x-lt>",
+                "<x-lt>Getting there!</x-lt>",
+                "<x-lt>Reach higher!</x-lt>"
             ],
             "captions":[
-                "<language>You've got a good base here. Why not try again to polish your skills?</language>",
-                "<language>You just made it past the finish line. You might want to practice some more.</language>",
-                "<language>Looks like you just made it over the hump. Try again to really gain some traction.</language>",
-                "<language>You know you can do it, now try again, and show everyone you can do even better!</language>",
-                "<language>Work hard. Practice hard. Possibilities are limitless!</language>"
+                "<x-lt>You've got a good base here. Why not try again to polish your skills?</x-lt>",
+                "<x-lt>You just made it past the finish line. You might want to practice some more.</x-lt>",
+                "<x-lt>Looks like you just made it over the hump. Try again to really gain some traction.</x-lt>",
+                "<x-lt>You know you can do it, now try again, and show everyone you can do even better!</x-lt>",
+                "<x-lt>Work hard. Practice hard. Possibilities are limitless!</x-lt>"
             ],
             "color":"#F2994A"
         },
@@ -68,22 +68,22 @@ window.strings = {
             "maxScore": 60,
             "minScore": 0,
             "headings":[
-                "<language>Need some help?</language>",
-                "<language>Keep going!</language>",
-                "<language>You want this!</language>",
-                "<language>Aim for the stars!</language>",
-                "<language>It is possible!</language>",
-                "<language>You can do it!</language>",
-                "<language>Exceed expectations!</language>"
+                "<x-lt>Need some help?</x-lt>",
+                "<x-lt>Keep going!</x-lt>",
+                "<x-lt>You want this!</x-lt>",
+                "<x-lt>Aim for the stars!</x-lt>",
+                "<x-lt>It is possible!</x-lt>",
+                "<x-lt>You can do it!</x-lt>",
+                "<x-lt>Exceed expectations!</x-lt>"
             ],
             "captions":[
-                "<language>Take a break and try again. You'll get there.</language>",
-                "<language>Never let anything bring you down. Give it another shot!</language>",
-                "<language>Find out what your destiny is, and then do it!</language>",
-                "<language>Fail fast and fail forward! You got this.</language>",
-                "<language>Nothing is impossible. The word itself says \"I'm Possible!\"</language>",
-                "<language>Bounce back from this and try again!</language>",
-                "<language>Start again and make a new, better, ending!</language>"
+                "<x-lt>Take a break and try again. You'll get there.</x-lt>",
+                "<x-lt>Never let anything bring you down. Give it another shot!</x-lt>",
+                "<x-lt>Find out what your destiny is, and then do it!</x-lt>",
+                "<x-lt>Fail fast and fail forward! You got this.</x-lt>",
+                "<x-lt>Nothing is impossible. The word itself says \"I'm Possible!\"</x-lt>",
+                "<x-lt>Bounce back from this and try again!</x-lt>",
+                "<x-lt>Start again and make a new, better, ending!</x-lt>"
             ],
             "color":"#EB5757"
         }
@@ -93,39 +93,39 @@ window.strings = {
             "maxScore": 100,
             "minScore": 90,
             "messages": [
-                "<language>You completed this area with great success.</language>",
-                "<language>No doubt about it, in this section you know your stuff.</language>",
-                "<language>After completing this section, itâ€™s obvious you know what you're talking about.</language>",
-                "<language>You completed this section with ease.</language>",
-                "<language>Looks like you have a strong grasp on this material.</language>",
-                "<language>You showed your skills in this area and finished with success.</language>",
-                "<language>You were really strong in this area.</language>"
+                "<x-lt>You completed this area with great success.</x-lt>",
+                "<x-lt>No doubt about it, in this section you know your stuff.</x-lt>",
+                "<x-lt>After completing this section, itâ€™s obvious you know what you're talking about.</x-lt>",
+                "<x-lt>You completed this section with ease.</x-lt>",
+                "<x-lt>Looks like you have a strong grasp on this material.</x-lt>",
+                "<x-lt>You showed your skills in this area and finished with success.</x-lt>",
+                "<x-lt>You were really strong in this area.</x-lt>"
             ]
         },
         {
             "maxScore": 89,
             "minScore": 70,
             "messages": [
-                "<language>You demonstrated competency in this area, but you might want to try again for a better understanding.</language>",
-                "<language>Your skills in this area are growing, but still need some improvement to reach your goal.</language>",
-                "<language>You might want to focus a bit here, thereâ€™s more room to grow in this area.</language>",
-                "<language>You're off to a good start, but thereâ€™s room to improve in this section.</language>",
-                "<language>You had some success in this section, but you may want to polish your skills here.</language>",
-                "<language>If you review the material for this area, we think you'll do even better next time.</language>",
-                "<language>Your performance in this section was sufficient but could be stronger.</language>"
+                "<x-lt>You demonstrated competency in this area, but you might want to try again for a better understanding.</x-lt>",
+                "<x-lt>Your skills in this area are growing, but still need some improvement to reach your goal.</x-lt>",
+                "<x-lt>You might want to focus a bit here, thereâ€™s more room to grow in this area.</x-lt>",
+                "<x-lt>You're off to a good start, but thereâ€™s room to improve in this section.</x-lt>",
+                "<x-lt>You had some success in this section, but you may want to polish your skills here.</x-lt>",
+                "<x-lt>If you review the material for this area, we think you'll do even better next time.</x-lt>",
+                "<x-lt>Your performance in this section was sufficient but could be stronger.</x-lt>"
             ]
         },
         {
             "maxScore": 69,
             "minScore": 0,
             "messages": [
-                "<language>The competencies you showed in this area could be stronger.</language>",
-                "<language>Looks like you could use a break to regroup and try again.</language>",
-                "<language>Thereâ€™s a fair amount of room for you to improve in this section.</language>",
-                "<language>This area was not the strongest, so give it another shot.</language>",
-                "<language>You should spend some more time reviewing this area and try again.</language>",
-                "<language>Check your source material and try again, there's room to improve.</language>",
-                "<language>You need to work on this area to turn it into a victory.</language>"
+                "<x-lt>The competencies you showed in this area could be stronger.</x-lt>",
+                "<x-lt>Looks like you could use a break to regroup and try again.</x-lt>",
+                "<x-lt>Thereâ€™s a fair amount of room for you to improve in this section.</x-lt>",
+                "<x-lt>This area was not the strongest, so give it another shot.</x-lt>",
+                "<x-lt>You should spend some more time reviewing this area and try again.</x-lt>",
+                "<x-lt>Check your source material and try again, there's room to improve.</x-lt>",
+                "<x-lt>You need to work on this area to turn it into a victory.</x-lt>"
             ]
         }
     ]
