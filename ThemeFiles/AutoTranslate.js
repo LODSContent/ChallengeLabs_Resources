bf44240efed6e2665adc273e038d535e19c6eca5
markmorganskillable <127998103+markmorganskillable@users.noreply.github.com>
2025-03-09 13:40:28 -0400

    Update AutoTranslate.js

--- ThemeFiles/AutoTranslate.js
+++ ThemeFiles/AutoTranslate.js
@@ -1,3 +1,11 @@
+/*
+ * Script Name: AutoTranslate.js
+ * Authors: Mark Morgan, Grok 3 (xAI)
+ * Version: 1.10
+ * Date: March 08, 2025
+ * Description: Translates elements in the HTML to the target language.
+ */
+
 // Begin Translation code
 
 // Get AutoTranslate setting
