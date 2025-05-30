5bf264aafd6ac11a5c67353350e553129290c86d
markmorganskillable <127998103+markmorganskillable@users.noreply.github.com>
2025-05-30 13:10:12 -0400

    Update GetInteractiveTokenPBI.ps1

--- MgGraph/GetInteractiveTokenPBI.ps1
+++ MgGraph/GetInteractiveTokenPBI.ps1
@@ -25,7 +25,7 @@ function main {
     # Authenticate with Azure AD
     try {
         # Use device code flow for interactive login
-        Connect-AzAccount -Tenant $tenant -ErrorAction Stop
+        Connect-AzAccount -Tenant $tenant
         if ($scriptDebug) { Write-Output "Authenticated to Azure AD." }
     }
     catch {
@@ -38,7 +38,7 @@ function main {
     try {
         $scopes = "https://analysis.windows.net/powerbi/api/.default"
         $clientId = "1950a258-227b-4e31-a9cf-717495945fc2"  # Azure PowerShell client ID
-        Connect-MgGraph -ClientId $clientId -Scopes $scopes -TenantId $tenant -ErrorAction Stop
+        Connect-MgGraph -ClientId $clientId -Scopes $scopes -TenantId $tenant
         if ($scriptDebug) { Write-Output "Authenticated to Power BI." }
 
         # Retrieve Power BI access token
