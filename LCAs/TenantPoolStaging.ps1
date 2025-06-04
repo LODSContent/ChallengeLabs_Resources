b345276aaf8b60b75c83be4266a455ad670fcab8
markmorganskillable <127998103+markmorganskillable@users.noreply.github.com>
2025-06-03 23:16:32 -0400

    Update TenantPoolStaging.ps1

--- LCAs/TenantPoolStaging.ps1
+++ LCAs/TenantPoolStaging.ps1
@@ -2,6 +2,7 @@
    Title: Create TAP enabled user, establish credential variables and configure the Tenant for lab use
    Description: Creates a new user with a Temporary Access Password and establishes credential variables for the lab. Also configures the Tenant for lab use.
    Target: Cloud Subscription - PowerShell - 7.4.0 | Az 11.1.0 (RC)
+   Version: 2025.06.03.2316
 #>
 
 param (
