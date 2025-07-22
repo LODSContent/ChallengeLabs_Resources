30dbe93d34b418496238fe96239a430bafd95b30
markmorganskillable <127998103+markmorganskillable@users.noreply.github.com>
2025-07-22 15:17:16 -0400

    Update TenantPoolPostCleanup.ps1

--- LCAs/TenantPoolPostCleanup.ps1
+++ LCAs/TenantPoolPostCleanup.ps1
@@ -45,6 +45,7 @@ if ($TenantName -eq $null -or $TenantName -eq "" -or $TenantName -like "@lab.Var
 } 
 
 #try {
+	if ($ScriptDebug) { Send-DebugMessage "Attempting Authentication to: $TenantName as: $AppName in the TenantPoolCleanup script." }
 	# MgGraph Authentication block (Cloud Subscription Target)
 	$AccessToken = (Get-AzAccessToken -ResourceUrl "https://graph.microsoft.com" -TenantId $TenantName).Token
 	$SecureToken = ConvertTo-Securestring $AccessToken -AsPlainText -Force
