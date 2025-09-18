ce543299dbc356a83e36250333d36ad59b735326
markmorganskillable <127998103+markmorganskillable@users.noreply.github.com>
2025-09-18 11:00:08 -0400

    Update TenantPoolStaging-Dev.ps1

--- LCAs/TenantPoolStaging-Dev.ps1
+++ LCAs/TenantPoolStaging-Dev.ps1
@@ -102,7 +102,7 @@ if (!$SkipCleanup) {
 	if ($ScriptDebug) { Send-DebugMessage "Sending ScriptingAppId: $ScriptingAppId - and ScriptingAppSecret: $ScriptingAppSecret" }
  
 	# URL of the script on GitHub
-	$scriptUrl = "https://raw.githubusercontent.com/LODSContent/ChallengeLabs_Resources/refs/heads/master/LCAs/TenantPoolPostCleanup.ps1"
+	$scriptUrl = "https://raw.githubusercontent.com/LODSContent/ChallengeLabs_Resources/refs/heads/master/LCAs/TenantPoolPostCleanup-Dev.ps1"
 	
 	# Fetch the script content using Invoke-WebRequest
 	$scriptBlock = [ScriptBlock]::Create((Invoke-WebRequest -Uri $scriptUrl -UseBasicParsing).Content)
