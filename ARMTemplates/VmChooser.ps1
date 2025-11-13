75e8d6ee5517c55f124db88402948ab7521ae53a
markmorganskillable <127998103+markmorganskillable@users.noreply.github.com>
2025-11-12 23:07:49 -0500

    Update VmChooser.ps1

--- ARMTemplates/VmChooser.ps1
+++ ARMTemplates/VmChooser.ps1
@@ -12,6 +12,8 @@ Write-Output "[INFO] Starting VM size selection for location: $location"
 # Authenticate with current identity
 # Connect-AzAccount -Identity | Out-Null
 
+Get-AzContext
+
 <# Try to set the subscription context
 try {
     Write-Output "[INFO] Attempting to set context with SubscriptionId: $subscriptionId"
