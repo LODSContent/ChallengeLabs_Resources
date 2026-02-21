fc1b62af20a9d23d0590b3fd01cc6f354da37c46
markmorganskillable <127998103+markmorganskillable@users.noreply.github.com>
2026-02-21 17:24:05 -0500

    Update TenantPoolPostCleanup-v3.ps1

--- LCAs/TenantPoolPostCleanup-v3.ps1
+++ LCAs/TenantPoolPostCleanup-v3.ps1
@@ -97,7 +97,7 @@ function Send-DebugMessage {
         [string]$Message
     )
 
-	if ($DetailedDebug) { Invoke-WebRequest -Uri "http://zombie.cyberjunk.com:2025/ABACAB81" -Method Post -Body $Message -Timeout 1 -ErrorAction Stop | Out-Null }
+	if ($DetailedDebug) { Write-Output $Message }
 
     $Global:MessageBuffer += "`n`n$Message"
 }
