91d8bbe5327902bb633874cff6f918212965c524
markmorganskillable <127998103+markmorganskillable@users.noreply.github.com>
2026-02-22 01:17:40 -0500

    Update TenantPoolStaging-v3.ps1

--- LCAs/TenantPoolStaging-v3.ps1
+++ LCAs/TenantPoolStaging-v3.ps1
@@ -823,7 +823,6 @@ LoriP,Lori,Penor,Lori Penor,Finance,Boston,MA,Manager
    if ($SubscriptionId) {
    		try {
 			if ($scriptDebug) { Send-DebugMessage "Subscription ID found. Cleaning Subscription." }
-<#
 			# Remove and re-add Owner Role to the lab user
 			try {
 			    Remove-AzRoleAssignment -SignInName "$TapUser" -RoleDefinitionName "Owner" -Scope "/subscriptions/$SubscriptionId" -ErrorAction Stop | Out-Null
@@ -838,7 +837,7 @@ LoriP,Lori,Penor,Lori Penor,Finance,Boston,MA,Manager
 			} catch {
 			    if ($scriptDebug) { Send-DebugMessage "Failed to set the Owner role for $TapUser." }
 	  		}
-#>
+<#
 			# Remove all Resource Groups
 			try {
 	  		    if ($scriptDebug) { Send-DebugMessage "Removing resource groups." }
@@ -853,6 +852,7 @@ LoriP,Lori,Penor,Lori Penor,Finance,Boston,MA,Manager
 			} catch {
 				if ($scriptDebug) { Send-DebugMessage "Resource Groups not found." }
 			}
+#>
 		} catch {
 			if ($scriptDebug) { Send-DebugMessage "Failed to clean and configure Trial Subscription." }
 	 	}
