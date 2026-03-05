389ac1ea0889d2c7427cace0d98b37440fade122
markmorganskillable <127998103+markmorganskillable@users.noreply.github.com>
2026-03-05 16:20:00 -0500

    Update EOLReport.js

--- ThemeFiles/EOLReport.js
+++ ThemeFiles/EOLReport.js
@@ -6,6 +6,54 @@
  * Description: Translates elements in the HTML to the target language.
  */
 
+// Helper Functions
+function getLabVariable(name) {
+    try {
+        if (debug) { console.log(`Retrieving lab variable - ${name}`); }
+        try{
+			clientAPI = window.api.v1;
+		} catch(e) {
+			clientAPI = null;
+		}
+        if (clientAPI) {
+            if (debug) { console.log(`API: Getting lab variable - ${name}`); }             
+            let value = window.api.v1.getLabVariable(name)?.toLowerCase() || null;
+            if (debug) { console.log(`API: Retrieved lab variable - ${name} = ${value}`); }
+            return value;
+        } else {
+            if (debug) { console.log(`NoAPI: Getting lab variable - ${name}`); }            
+            let checkName = name.toLowerCase();
+            let value = $('[data-name]').filter(function() { return $(this).attr('data-name').toLowerCase() == checkName }).val().toLowerCase();
+            if (debug) { console.log(`NoAPI: Retrieved lab variable - ${name} = ${value}`); }
+            return value;
+        }
+    } catch (e) {
+        if (debug) { console.log(`Failed to retrieve lab variable - ${name}`); }
+        return null;
+    }
+}
+
+function setLabVariable(name, value) {
+    try {
+        if (debug) { console.log(`Setting lab variable ${name} to ${value}`); }
+        try{
+			clientAPI = window.api.v1;
+		} catch(e) {
+			clientAPI = null;
+		}       
+        if (clientAPI) {
+            window.api.v1.setLabVariable(name,value);
+        } else {
+            $('[data-name="' + name + '"]').val(value).trigger("change");
+        }
+    } catch (e) {
+        if (debug) { console.log(`Failed to set lab variable ${name} to ${value}`); }
+    }
+}
+
+// Initialize the debug variable from the lab variable
+debug = ["true", "yes"].includes((getLabVariable("Debug") ?? getLabVariable("debug") ?? "").trim().toLowerCase());
+
 // Runs at the end of the lab report
 if (debug) { console.log("Starting end-of-lab report generation"); }
 document.querySelector("body").style.display = "none"; // Blank screen initially
@@ -68,17 +116,6 @@ try {
     document.querySelector('body.end-of-lab-report')?.setAttribute('style', 'display:block !important');
     if (debug) { console.log("Standard report revealed due to error"); }
 }
-
-// Helper Functions
-
-// Retrieve a lab variable listed in Markdown as a case-insensitive variable name
-//function getLabVariable(name) {
-//    if (debug) { console.log(`Leaderboard: Retrieving lab variable - ${name}`); }
-//    let checkName = name.toLowerCase();
-//    let value = $('[data-name]').filter(function() { return $(this).attr('data-name').toLowerCase() == checkName }).val();
-//    if (debug) { console.log(`Leaderboard: Retrieved value for ${name} - ${value}`); }
-//    return value;
-//}
 
 function getLabLanguageCode() {
     return document.documentElement.lang || "en-US";
